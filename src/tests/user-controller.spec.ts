753cd6f8777b4801056e1120b3ec53c20e3357d5
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-13 17:47:40 -0300

    implement first jest test

---
+++ src/tests/user-controller.spec.ts
@@ -0,0 +1,24 @@
+import { EmailService } from '../services/EmailService'
+import { app } from '../index'
+import request from 'supertest'
+
+describe('Email service use case', () => {
+  it('It should send email', async () => {
+    const repository = {
+      mailTo: {
+        name: 'Bruno Corrêa', email: 'bruno@example.com'
+      },
+      subject: {
+        subject: 'Typescript', body: 'Typescript is really cool'
+      }
+    }
+    
+    const response = await request(app).get('/users/create', () => {
+      const emailService = new EmailService()
+      emailService.sendMail(
+        repository.mailTo, repository.subject
+      )
+    })
+    expect(response).toBeTruthy()
+  })
+})
